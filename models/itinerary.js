ef9a4a97fa89a8e34fca97feda89866690511c92
wcheung26 <william.cheung94@gmail.com>
2017-08-01 12:43:05 -0700

    adding completed models

---
+++ models/itinerary.js
@@ -0,0 +1,24 @@
+module.exports = function(sequelize, DataTypes) {
+  var Itinerary = sequelize.define("itineraries", {
+    itinerary_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validation: {
+        len: [1]
+      }
+    }
+  });
+
+  Itinerary.associate = function(models) {
+    Itinerary.belongsTo(models.users, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+
+    User.hasMany(models.activities, {
+      onDelete: 'CASCADE'
+    });
+  };
+  return Itinerary;
+};
