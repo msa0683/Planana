dc7f3098859152431a54566ee45c90af5321cf3f
wcheung26 <william.cheung94@gmail.com>
2017-08-02 13:53:30 -0700

    linking form data to get route

--- controllers/api_controller.js
+++ controllers/api_controller.js
@@ -6,9 +6,22 @@ var router = express.Router();
 var sget = require('simple-get');
 
 
-router.get("/:city/:type", function(req, res) {
-  var city = req.params.city;
-  var type = req.params.type;
+router.get("/activities", function(req, res) {
+  var formInput = req.query;
+  var city = formInput.cityName;
+  var options = Object.keys(formInput);
+  var type = '';
+  for (var i = 0; i < options.length; i++) {
+    if(options[i] !== 'cityName') {
+      if (i === options.length - 1) {
+        type += options[i];
+      } else {
+        type += options[i] + "|";
+      }
+    }
+  }
+  console.log("type:", type)
+
   var query = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=" + city + "&type=" + type + "&key=AIzaSyDAWpS82SOzjGchLjXznjhyCoVC8zxQw_s";
   sget.concat(query, function(err, response, data) {
     if (err) throw err;
