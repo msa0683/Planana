c23606df98e31e4d103a158cbf7a1bfa001bdbd2
wcheung26 <william.cheung94@gmail.com>
2017-08-03 01:19:20 -0700

    add categories to google results object and remove hiking category

--- controllers/api_controller.js
+++ controllers/api_controller.js
@@ -25,8 +25,28 @@ router.get("/activities", function(req, res) {
   var query = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=" + city + "&type=" + type + "&key=AIzaSyDAWpS82SOzjGchLjXznjhyCoVC8zxQw_s";
   sget.concat(query, function(err, response, data) {
     if (err) throw err;
-    console.log(JSON.parse(data.toString('utf-8')));
-    res.json(JSON.parse(data.toString('utf-8')));
+    var allResults = JSON.parse(data.toString('utf-8'));
+    var parkList = [];
+    var museumList = [];
+    var restaurantsList = [];
+    var shoppingList = [];
+    var nightlifeList = [];
+    for (var i = 0; i < allResults.results.length; i++) {
+      if (allResults.results[i].types.indexOf('park') >= 0) parkList.push(allResults.results[i]);
+      if (allResults.results[i].types.indexOf('museum') >= 0) museumList.push(allResults.results[i]);
+      if (allResults.results[i].types.indexOf('restaurant') >= 0 || allResults.results[i].types.indexOf('cafe') >= 0) restaurantsList.push(allResults.results[i]);
+      if (allResults.results[i].types.indexOf('shopping_mall') >= 0 || allResults.results[i].types.indexOf('department_store') >= 0) shoppingList.push(allResults.results[i]);
+      if (allResults.results[i].types.indexOf('night_club') >= 0 || allResults.results[i].types.indexOf('bar') >= 0) nightlifeList.push(allResults.results[i]);
+     }
+    var allLists = {
+      parks: parkList,
+      museums: museumList,
+      restaurants: restaurantsList,
+      malls: shoppingList,
+      nightlife: nightlifeList
+    };
+    // console.log(allLists)
+    res.json(allLists);
   })
 });
 
