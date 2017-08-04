9eb69f004c26a20dc750ac2ff067b7afacf8f92c
wcheung26 <william.cheung94@gmail.com>
2017-08-04 01:32:05 -0700

    add results page after creating new itinerary

--- controllers/itinerary_controller.js
+++ controllers/itinerary_controller.js
@@ -12,7 +12,79 @@ var imageSearch = new GoogleImages('014302329711232858745:kmu70nd-fok', 'AIzaSyB
 //     var cityImage = images[0].url;
 //   	res.render("index2", {cityImage: cityImage});
 // });
+router.get("/:username", function(req,res) {
+	var username = req.params.username;
+	// Display user's saved itineraries
+	// db.activities.findAll({
+	// 	include: [{
+	// 		model: db.itineraries,
+	// 		as: 'itinerary',
+	// 		where: {
+	// 			userId: 1
+	// 		}
+	// 	}]
+	// }).then(function(result) {
+	// 	console.log(result);
+	// 	for (var i = 0; i < result.length; i++) {
+	// 			if (result[i].itineraryId) {}result[i]
+	// 		}		
+	// 	var hbsObj = {
+	// 		itineraries: result
+	// 	}
+	// 	res.render('saved', hbsObj);
+	// })
 
+	db.itineraries.findAll({
+		include: [{
+			model: db.users,
+			as: 'user',
+			where: {
+				username: username
+			}
+		}]
+	}).then(function(result) {
+		console.log(result);
+		console.log('username:', username)
+		var hbsObj = {
+			itineraries: result,
+			username: username
+		}
+		// for (var i = 0; i < result.length; i++) {
+		// 	console.log("result id:", result[i].id);
+		// 	db.activities.findAll({
+		// 		where: {
+		// 			itineraryId: result[i].id
+		// 		}
+		// 	}).then(function(activities) {
+
+		// 	})
+		// }
+		console.log(hbsObj);
+		res.render('saved', hbsObj);
+	})
+});
+
+router.get("/:username/:itineraryId", function(req, res) {
+	var itineraryId = req.params.itineraryId;
+	var hbsObj = {};
+	db.itineraries.findOne({
+		where: {
+			id: itineraryId
+		}
+	}).then(function(itinObj) {
+		console.log("itinObj", itinObj);
+		hbsObj.itinerary_name = itinObj.itinerary_name
+
+		db.activities.findAll({
+			where: {
+				itineraryId: itineraryId
+			}
+		}).then(function(result){
+			hbsObj.activities = result;
+			res.render('itinerary', hbsObj);
+		})
+	})
+})
 
 router.post("/:username", function(req, res) {
 	console.log("request", req.body);
@@ -37,10 +109,10 @@ router.post("/:username", function(req, res) {
 					if (body.photo_ref[i] === null) body.photo_ref[i] = "";
 					db.activities.create({activity_name: body.place_name[i], activity_address: body.place_address[i], activity_photo: body.photo_ref[i], itineraryId: itineraryID}).then(function() {});
 				}
-		    	res.redirect("/");
+		    	res.redirect("/itinerary/" + username);
 			} else {
 				db.activities.create({activity_name: body.place_name, activity_address: body.place_address, activity_photo: body.photo_ref, itineraryId: itineraryID}).then(function() {
-			      res.redirect("/");
+			      res.redirect("/itinerary/" + username);
 			    });
 			}
 		})	
